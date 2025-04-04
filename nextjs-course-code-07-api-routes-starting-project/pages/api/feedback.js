cd08ef895aa7761f8f2cc583e8032dd1fb486d28
sailakshmy <sailakshmy94@gmail.com>
2025-04-04 06:41:12 +0400

    Parsed the incoming requests via server-side code

--- nextjs-course-code-07-api-routes-starting-project/pages/api/feedback.js
+++ nextjs-course-code-07-api-routes-starting-project/pages/api/feedback.js
@@ -1,7 +1,27 @@
+import fs from "fs";
+import path from "path";
 function handler(req, res) {
-  res.status(200).json({
-    message: "This works!",
-  });
+  if (req.method === "POST") {
+    const { email, text } = req.body;
+    const feedbackText = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    };
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(feedbackText);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({
+      message: "Success",
+      feedback: feedbackText,
+    });
+  } else {
+    res.status(200).json({
+      message: "This works!",
+    });
+  }
 }
 
 export default handler;
