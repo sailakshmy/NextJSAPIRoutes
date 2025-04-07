91b3d6e5a842b316cc0289cf9905cda2f8cb061a
sailakshmy <sailakshmy94@gmail.com>
2025-04-07 07:02:22 +0400

    Created Dynamic API routes

---
+++ nextjs-course-code-07-api-routes-starting-project/pages/api/[feedbackId].js
@@ -0,0 +1,11 @@
+import { buildFeedbackPath, extractFeedback } from "./feedback";
+
+export default function handler(req, res) {
+  const feedbackId = req.query.feedbackId;
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  const feedback = data?.find((item) => item?.id === feedbackId);
+  res.status(200).json({
+    feedback,
+  });
+}
--- nextjs-course-code-07-api-routes-starting-project/pages/feedback/index.js
+++ nextjs-course-code-07-api-routes-starting-project/pages/feedback/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 const Feedback = (props) => {
+  const [feedbackDetails, setFeedbackDetails] = useState();
+  const showFeedbackDetails = (id) => {
+    fetch(`/api/${id}`)
+      .then((data) => data.json())
+      .then((data) => setFeedbackDetails(data.feedback));
+  };
   return (
-    <ul>
-      {props.feedbackItems.map((feedback) => (
-        <li key={feedback.id}>{feedback.text}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {props.feedbackItems.map((feedback) => (
+          <li key={feedback.id}>
+            {feedback.text}
+
+            <button onClick={showFeedbackDetails.bind(null, feedback.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+      {feedbackDetails && <p>{feedbackDetails.email}</p>}
+    </>
   );
 };
 
